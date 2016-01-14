09294d45c5b7d5c9dd0be8e58c95167564a9ae96
tianzx <436011080@qq.com>
2016-01-14 10:48:40 +0800

    'success'

--- js/decorator/decorator.js
+++ js/decorator/decorator.js
@@ -8,13 +8,22 @@ function MacBook(name) {
 	this.cost = function() {
 		return 1000;
 	}
+	console.log(this.cost());
 }
 
 function Ram(Macbook) {
 	this.cost = function() {
 		return Macbook.cost() + 50;
 	}
+	console.log(this.cost());
 }
 
-var price = new Ram(new MacBook("my")).cost();
-console.log(price);
+function Mounse(Macbook) {
+	this.cost = function() {
+		return Macbook.cost() + 10;
+	}
+	console.log(this.cost());
+}
+//var price = new Mounse(new Ram(new MacBook("my"))).cost();
+//console.log(price);
+var price = new Ram(new Mounse(new MacBook()));
