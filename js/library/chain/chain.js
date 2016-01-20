9bf034f5b5cc02392da4b39e442f9fdfc44fefbe
tianzx <436011080@qq.com>
2016-01-20 17:02:42 +0800

    finish

--- js/library/chain/chain.js
+++ js/library/chain/chain.js
@@ -1,3 +1,6 @@
+/****
+ * nodejs test window will make error
+ */
 // Include syntactic sugar to help the development of our interface.
 Function.prototype.method = function(name, fn) {
   this.prototype[name] = fn;
@@ -5,56 +8,20 @@ Function.prototype.method = function(name, fn) {
 };
 (function() {
   function _$(els) {
-    // ...
   }
-  	/*
-	 * Events addEvent getEvent
-	 */
   _$.method('addEvent', function(type, fn) {
-    // ...
   }).method('getEvent', function(e) {
-    // ...
-  }).
-  /*
-	 * DOM addClass removeClass replaceClass hasClass getStyle setStyle
-	 */
-  method('addClass', function(className) {
-    // ...
+  }).method('addClass', function(className) {
   }).method('removeClass', function(className) {
-    // ...
   }).method('replaceClass', function(oldClass, newClass) {
-    // ...
   }).method('hasClass', function(className) {
-    // ...
   }).method('getStyle', function(prop) {
-    // ...
   }).method('setStyle', function(prop, val) {
-    // ...
-  }).
-  	/*
-	 * AJAX load. Fetches an HTML fragment from a URL and inserts it into an
-	 * element.
-	 */
-  method('load', function(uri, method) {
-    // ...
+  }).method('load', function(uri, method) {
   });
   window.$ = function() {
     return new _$(arguments);
-  });
-})();
-
-Function.prototype.method = function(name, fn) {
-  // ...
-};
-(function() {
-  function _$(els) {
-    // ...
   }
-  _$.method('addEvent', function(type, fn) {
-    // ...
-  })
-  // ...
-    
   window.installHelper = function(scope, interface) {
     scope[interface] = function() {
       return new _$(arguments);
@@ -67,9 +34,6 @@ Function.prototype.method = function(name, fn) {
 
 installHelper(window, '$');
 
-$('example').show();
-
-
 /* Another usage example. */
 
 // Define a namespace without overwriting it if it already exists.
@@ -81,7 +45,7 @@ installHelper(com.example.util, 'get');
 
 (function() {
   var get = com.example.util.get;
-  get('example').addEvent('click', function(e) {
+  get('test1').addEvent('click', function(e) {
     get(this).addClass('hello');
   });
 })();
