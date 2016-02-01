383f50341a3597ff8ac2f8d0b233be05e2b5022a
tianzx <436011080@qq.com>
2016-02-01 15:49:51 +0800

    finish

---
+++ js/app/chain/chainCall.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ * @param {0-99} id
+ */
+var processor1 = function(id) {
+	if(id>100 ||id<0) {
+		return "nextProcessor";
+	}else {
+		console.log(id);
+		return "success"
+	}
+}
+/**
+ * 
+ * @param {100-199} id
+ */
+var processor2 = function(id) {
+	if(id>199 ||id<100) {
+		return "nextProcessor";
+	}else {
+		console.log(id);
+		return "success"
+	}
+}
+/**
+ * 
+ * @param {200-299} id
+ */
+var processor3 = function(id) {
+	if(id>299 ||id<200) {
+		return "nextProcessor";
+	}else {
+		console.log(id);
+		return "success"
+	}
+}
+
+
+var Chain = function() {
+	this.processorList = [];
+	this.processorList.push(processor1);
+	this.processorList.push(processor2);
+	this.processorList.push(processor3);
+}
+Chain.prototype.process = function(id) {
+	for(var i=0;i<this.processorList.length;i++){
+		if(this.processorList[i].call(this,id)=='nextProcessor') {
+			console.log("need next processor ");
+		}else{
+			break;
+		}
+	}
+}
+var chain = new Chain();
+chain.process("60");
